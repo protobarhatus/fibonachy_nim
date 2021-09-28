3daea034746f700d1e127169809b7d0f9dd426d0
Олег <suhodolov.o@yandex.ru>
2021-09-28 21:57:07 +0300

    fixes

--- sudoku.c
+++ sudoku.c
@@ -4,11 +4,15 @@
 #include <stdbool.h>
 #include <assert.h>
 #include <stdarg.h>
+#include <ctype.h>
+#include <math.h>
 
+#define SUDOKU_MAX_SIZE 9
+#define sqr(X) ((X)*(X))
 struct Array2d
 {
     int w, h;
-    int arr[81];
+    int arr[sqr(SUDOKU_MAX_SIZE)];
 };
 
 struct  Array2d defaultArray2d(int w, int h)
@@ -16,18 +20,18 @@ struct  Array2d defaultArray2d(int w, int h)
     struct Array2d arr;
     arr.w = w;
     arr.h = h;
-    memset((void *)arr.arr, 0, sizeof(int) * 81);
+    memset((void *)arr.arr, 0, sizeof(int) * sqr(SUDOKU_MAX_SIZE));
     return arr;
 }
 int * atArray2d(struct  Array2d * obj, int x, int y)
 {
     return &obj->arr[x * obj->w + y];
 }
-#define BOOL_ARR_SIZE 10
+#define BOOL_ARR_SIZE (SUDOKU_MAX_SIZE + 1)
 struct Array2dBoolArray
 {
     int w, h;
-    bool arr[BOOL_ARR_SIZE * 81];
+    bool arr[BOOL_ARR_SIZE * sqr(SUDOKU_MAX_SIZE)];
 };
 
 struct Array2dBoolArray defaultArray2dBoolArray(int w, int h, bool def_value)
@@ -35,7 +39,7 @@ struct Array2dBoolArray defaultArray2dBoolArray(int w, int h, bool def_value)
     struct Array2dBoolArray arr;
     arr.w = w;
     arr.h = h;
-    memset((void *)arr.arr, def_value, sizeof(bool) * 81 * BOOL_ARR_SIZE);
+    memset((void *)arr.arr, def_value, sizeof(bool) * sqr(SUDOKU_MAX_SIZE) * BOOL_ARR_SIZE);
     return arr;
 }
 bool * atArray2dBoolArray(struct Array2dBoolArray * obj, int i, int j)
@@ -49,7 +53,8 @@ struct PointerTuple
     void ** pointers;
     int size;
 };
-
+//va_list may seem to be extra but like I cannot foresee how many times and how I want to use it
+//and so I want some generalized way to emulate lambda captures that I can save and use for the future
 struct PointerTuple constructPointerTuple(int size, ...)
 {
     int i = 0;
@@ -66,11 +71,11 @@ struct PointerTuple constructPointerTuple(int size, ...)
     va_end(pointers);
     return tuple;
 }
+
 void destructPointerTuple(struct PointerTuple * obj)
 {
     free(obj->pointers);
 }
-
 
 void * atPointerTuple(struct PointerTuple * obj, int i)
 {
@@ -78,35 +83,33 @@ void * atPointerTuple(struct PointerTuple * obj, int i)
     return obj->pointers[i];
 }
 
-
 struct Sudoku
 {
     struct Array2d sudoku;
     struct Array2dBoolArray possible_states;
 };
 
-void assignValue(int i, int j, int value, struct Sudoku * sud);
+bool assignValue(int i, int j, int value, struct Sudoku * sud);
 
-bool isDigit(char s)
+struct Sudoku initializeSudoku(FILE * input, int w)
 {
-    return s >= '0' && s <= '9';
-}
-
-struct Sudoku initializeSudoku(FILE * input)
-{
+    int h = w;
     struct Sudoku obj;
     char c_read = 0, i = 0, j = 0;
-    obj.sudoku = defaultArray2d(9, 9);
-    obj.possible_states = defaultArray2dBoolArray(9, 9, true);
+    assert(w <= SUDOKU_MAX_SIZE);
+    assert(h <= SUDOKU_MAX_SIZE);
+
+    obj.sudoku = defaultArray2d(w, h);
+    obj.possible_states = defaultArray2dBoolArray(w, h, true);
    // printf("start loading\n");
-    for (i = 0; i < 9; ++i)
+    for (i = 0; i < w; ++i)
     {
-        for (j = 0; j < 9; ++j)
+        for (j = 0; j < h; ++j)
         {
            // printf("%d %d\n", i, j);
             do {
                 fscanf(input, "%c", &c_read);
-            } while (!isDigit(c_read));
+            } while (!isdigit(c_read));
             assignValue(i, j, c_read - '0', &obj);
         }
     }
@@ -143,9 +146,9 @@ bool isComplete(struct Sudoku * obj)
 void outSudoku(struct Sudoku * obj, FILE * output)
 {
     int i = 0, j = 0;
-    for (i = 0; i < 9; ++i)
+    for (i = 0; i < obj->sudoku.w; ++i)
     {
-        for (j = 0; j < 9; ++j)
+        for (j = 0; j < obj->sudoku.h; ++j)
         {
             fprintf(output, "%d ", *atSudoku(obj, i, j));
         }
@@ -159,23 +162,25 @@ void doSmthToAllConnectedCells(int i_cur, int j_cur, struct Sudoku * sud, struct
 {
     int i = 0, j = 0;
     int sq_start_i = 0, sq_start_j = 0;
-    for (i = 0; i < 9; ++i)
+    int sqrt_size = round(sqrt(sud->sudoku.w));
+
+    for (i = 0; i < sud->sudoku.w; ++i)
     {
         if (i != i_cur)
             do_func(i, j_cur, *atSudoku(sud, i, j_cur), tupl_arg);
     }
-    for (j = 0; j < 9; ++j)
+    for (j = 0; j < sud->sudoku.h; ++j)
     {
         if (j != j_cur)
             do_func(i_cur, j, *atSudoku(sud, i_cur, j), tupl_arg);
     }
 
-    sq_start_i = (i_cur / 3) * 3;
-    sq_start_j = (j_cur / 3) * 3;
+    sq_start_i = (i_cur / sqrt_size) * sqrt_size;
+    sq_start_j = (j_cur / sqrt_size) * sqrt_size;
 
-    for (i = sq_start_i; i < sq_start_i + 3; ++i)
+    for (i = sq_start_i; i < sq_start_i + sqrt_size; ++i)
     {
-        for (j = sq_start_j; j < sq_start_j + 3; ++j)
+        for (j = sq_start_j; j < sq_start_j + sqrt_size; ++j)
         {
             //and is because cells from same line or column were already processed
             if (i != i_cur && j != j_cur)
@@ -213,17 +218,31 @@ void unsetPossibleValue(int i, int j, int cell_val, struct PointerTuple * args)
 {
     struct Array2dBoolArray * possibles = (struct Array2dBoolArray*) atPointerTuple(args, 0);
     int val = *(int*) atPointerTuple(args, 1);
+    bool * has_variants = (bool *) atPointerTuple(args, 2);
     atArray2dBoolArray(possibles, i, j)[val] = false;
+    *has_variants = *has_variants * amountOfPossibleVars(atArray2dBoolArray(possibles, i, j));
 }
-void assignValue(int i, int j, int value, struct Sudoku * sud)
+bool assignValue(int i, int j, int value, struct Sudoku * sud)
 {
+    bool are_all_cells_has_variants_left = true;
     *atArray2d(&sud->sudoku, i, j) = value;
   //printf("pointer: %i\n", &sud->possible_states);
-    struct PointerTuple arg = constructPointerTuple(2, (void *) &sud->possible_states, (void *)&value);
+    struct PointerTuple arg = constructPointerTuple(3, (void *) &sud->possible_states, (void *)&value,
+                                                    (void*)&are_all_cells_has_variants_left);
     doSmthToAllConnectedCells(i, j, sud, &arg, unsetPossibleValue);
     destructPointerTuple(&arg);
+    return are_all_cells_has_variants_left;
 }
-bool doSimpleLogicStep(struct Sudoku * sud)
+
+enum LogicStepResult_enum
+{
+    LOGIC_STEP_RESULT_OK,
+    LOGIC_STEP_RESULT_NO_PLACEMENTS,
+    LOGIC_STEP_RESULT_CONTRADICTION
+};
+typedef enum LogicStepResult_enum LogicStepResult;
+
+LogicStepResult doSimpleLogicStep(struct Sudoku * sud)
 {
     int i_proc = 0, j_proc = 0;
     int value = 0;
@@ -236,20 +255,22 @@ bool doSimpleLogicStep(struct Sudoku * sud)
             if (isCellUndefined(i_proc, j_proc, sud) &&
                 (value = hasOnlyOnePossibleVar(atArray2dBoolArray(&sud->possible_states, i_proc, j_proc))))
             {
-                assignValue(i_proc, j_proc, value, sud);
-                return true;
+                return (assignValue(i_proc, j_proc, value, sud) ? LOGIC_STEP_RESULT_OK : LOGIC_STEP_RESULT_CONTRADICTION);
             }
         }
     }
-    return false;
+    return LOGIC_STEP_RESULT_NO_PLACEMENTS;
 }
 
-bool trySimpleLogicApproach(struct Sudoku * sud)
+LogicStepResult trySimpleLogicApproach(struct Sudoku * sud)
 {
     bool did_something = false;
-    while (doSimpleLogicStep(sud))
+    LogicStepResult step_res = LOGIC_STEP_RESULT_OK;
+    while ((step_res = doSimpleLogicStep(sud)) == LOGIC_STEP_RESULT_OK)
         did_something = true;
-    return did_something;
+    if (step_res == LOGIC_STEP_RESULT_CONTRADICTION)
+        return step_res;
+    return did_something ? LOGIC_STEP_RESULT_OK : LOGIC_STEP_RESULT_NO_PLACEMENTS;
 }
 
 struct Pair
@@ -289,9 +310,12 @@ struct Pair cellWithMinAmountOfPossibles(struct Sudoku * sudoku)
 
 bool completeSudoku(struct Sudoku * sudoku);
 
+
+
 bool tryVariants(struct Sudoku * sudoku)
 {
     struct Pair cell = cellWithMinAmountOfPossibles(sudoku);
+
     if (cell.first == -1)
         return false;
 
@@ -304,7 +328,8 @@ bool tryVariants(struct Sudoku * sudoku)
         if (its_possibles[i])
         {
             variants[sud_counter] = *sudoku;
-            assignValue(cell.first, cell.second, i, &variants[sud_counter]);
+            if (!assignValue(cell.first, cell.second, i, &variants[sud_counter]))
+                return false;
             ++sud_counter;
         }
     }
@@ -323,7 +348,8 @@ bool tryVariants(struct Sudoku * sudoku)
 
 bool completeSudoku(struct Sudoku * sudoku)
 {
-    trySimpleLogicApproach(sudoku);
+    if (trySimpleLogicApproach(sudoku) == LOGIC_STEP_RESULT_CONTRADICTION)
+        return false;
     if (!isComplete(sudoku))
         return tryVariants(sudoku);
     return true;
@@ -366,38 +392,11 @@ bool checkSudoku(struct Sudoku * sudoku)
 int main()
 {
     setbuf(stdout, 0);
-    struct Sudoku sudoku;
-    bool found_res = false;
-    bool res_right = false;
-    char prestr[100];
-    int sum = 0;
-    while (true)
-    {
-        printf("Ent:\n");
-        gets(prestr);
-        if (prestr[0] == 'S')
-            printf("%d\n", sum);
-        sudoku = initializeSudoku(stdin);
-        found_res = completeSudoku(&sudoku);
-        if (!found_res)
-            printf("ERROR: didn't found solution\n");
-        res_right = checkSudoku(&sudoku);
-        if (!res_right)
-        {
-            printf("ERROR: wrong res\n");
-            outSudoku(&sudoku, stdout);
-        }
-        if (found_res && res_right)
-            printf("Succesfully\n");
-        sum += *atSudoku(&sudoku, 0, 0) * 100 + *atSudoku(&sudoku, 0, 1) * 10 + *atSudoku(&sudoku, 0, 2);
-    }
-
-
-    /*struct Sudoku sudoku = initializeSudoku(stdin);
+    struct Sudoku sudoku = initializeSudoku(stdin, 9);
 
     printf("Loaded\n");
     completeSudoku(&sudoku);
 
-    outSudoku(&sudoku, stdout);*/
+    outSudoku(&sudoku, stdout);
     return 0;
 }
